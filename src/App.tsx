ce30b9e942e116da2906e0263d98c8655c2f0c66
Vinicius Gomes <vinicius.gomes@modalgr.com.br>
2024-03-15 16:18:01 -0300

    Configurando React Helmet Async

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@ import { Button } from './components/ui/button'
 import './global.css'
 import { router } from './routes'
 import {RouterProvider} from 'react-router-dom'
+import {HelmetProvider,Helmet} from 'react-helmet-async'
 export function App() {
   return (
     <>
+    <HelmetProvider>
+      <Helmet titleTemplate='%s | Pizza Shop'/>
       <RouterProvider router={router} />
+    </HelmetProvider>
     </>
   )
 }
